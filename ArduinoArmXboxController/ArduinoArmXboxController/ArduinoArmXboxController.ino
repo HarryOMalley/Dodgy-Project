669fa11a30818ba70ea584ae585f24ecbdd7b7af
Harry O'Malley <hazaomalley@gmail.com>
2017-11-14 13:11:19 +0000

    Added new solution to test the arm with the xbox controller.

---
+++ ArduinoArmXboxController/ArduinoArmXboxController/ArduinoArmXboxController.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ArduinoArmXboxController.ino
+ Created:	11/14/2017 1:09:42 PM
+ Author:	hazao
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}

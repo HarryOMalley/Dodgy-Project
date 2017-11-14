32ff95150a62f963518fd132b370efed2dda86db
Harry O'Malley <hazaomalley@gmail.com>
2017-11-14 13:08:40 +0000

    Code working to test all of the servos, not very useful other than to test that they are working.

--- ArduinoArmTest/ArduinoArmTest/ArduinoArmTest.ino
+++ ArduinoArmTest/ArduinoArmTest/ArduinoArmTest.ino
@@ -1,4 +1,4 @@
-/* Arm testing code to make the servos go crazy
+/* Arm testing code to make the servos go from lowest position to highest postion, only really useful to test if they are working.
 */
 
 #include <Servo.h>
@@ -9,19 +9,23 @@ Servo myservo3;		// twelve servo objects can be created on most boards
 Servo myservo4;
 Servo myservo5;
 Servo myservo6;
+Servo myservo7;
+Servo myservo8;
 
 float pos = 0;    // variable to store the servo position
 
 void setup() {
-	myservo.attach(8);		// attaches the servo owhat voltn pin 9 to the servo object
-	myservo2.attach(9);
-	myservo3.attach(10);
-	myservo4.attach(11);
-	myservo5.attach(12);
-	myservo6.attach(13);
+	myservo.attach(2);		// attaches the servo owhat voltn pin 9 to the servo object
+	myservo2.attach(3);
+	myservo3.attach(4);
+	myservo4.attach(5);
+	myservo5.attach(6);
+	myservo6.attach(7);
+	myservo7.attach(8);
+	myservo8.attach(9);
 
-	myservo.write(90);
-	myservo3.write(0);
+	myservo6.write(90);
+	myservo7.write(0);
 
 	Serial.begin(9600);
 }
@@ -29,22 +33,28 @@ void setup() {
 void loop() {
 	for (pos = 0; pos <= 180; pos += 2) { // goes from 0 degrees to 180 degrees
 										  // in steps of 1 degree
-										  // tell servo to go to position in variable 'pos'
+		myservo.write(pos);				  // tell servo to go to position in variable 'pos'
 		myservo2.write(pos);
-
+		myservo3.write(pos);
 		myservo4.write(pos);
 		myservo5.write(pos);
 		myservo6.write(pos);
+		myservo7.write(pos);
+		myservo8.write(pos);
+
 		delay(1);                       // waits 15ms for the servo to reach the position
 		Serial.println("Im doing something!");
 	}
 	for (pos = 180; pos >= 0; pos -= 2) { // goes from 180 degrees to 0 degrees
 										  //myservo.write(pos);              // tell servo to go to position in variable 'pos'
+		myservo.write(pos);				  // tell servo to go to position in variable 'pos'
 		myservo2.write(pos);
-		//myservo3.write(pos);
+		myservo3.write(pos);
 		myservo4.write(pos);
 		myservo5.write(pos);
 		myservo6.write(pos);
+		myservo7.write(pos);
+		myservo8.write(pos);
 		delay(1);                       // waits 15ms for the servo to reach the position
 		Serial.println("Im doing something different!");
 	}

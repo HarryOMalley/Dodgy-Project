978b0987e938c1abd4324670e50f92f2bd02087d
Harry O'Malley <hazaomalley@gmail.com>
2017-11-14 11:38:33 +0000

    Made a new project to test the xbox controller with the Arduino Shield

---
+++ ArduinoUSBXBOX/ArduinoUSBXBOX/ArduinoUSBXBOX.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ArduinoUSBXBOX.ino
+ Created:	11/14/2017 11:38:01 AM
+ Author:	hazao
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}

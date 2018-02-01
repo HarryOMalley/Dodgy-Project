714b9f3e87a37ddbb5827108f4a368313818cf0f
Kushal Pankhania <kushal171@googlemail.com>
2018-02-01 22:37:51 +0000

    arm + ultrasonic
    added code to allow entry into "stabilisation" mode where the arm can
    only be used to rotate the dial(knob)
    start button will flick the switch when in "stabilisation" mode and
    return to original position
    removed duplicate code from previous push
    ultrasonic sensor code set up to allow tri-track to stop at 10-12 cm
    from the pannel
    commented out code to change angles of rotation
    replaced regular rotation of servos from A, B, X, and Y with triggers
    that can be held down instead of pressing for 1 movment, has issue with
    dead zone still.
    // TODO: fix deadzones with triggers
    // TODO: create external cpp and header file along with constants for
    variables

--- ArduinoArmXboxController/ArduinoArmXboxController/ArduinoArmXboxController.ino
+++ ArduinoArmXboxController/ArduinoArmXboxController/ArduinoArmXboxController.ino
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <XBOXRECV.h>
 #include <Servo.h>
 
@@ -8,9 +7,18 @@
 #endif
 #include <SPI.h>
 
+// pins used for ultrasonic sensors
+#define trigPin 51
+#define echoPin 53
+
+#define neutral 90
+#define LRboard 20
+#define FBboard 20
+
 // Defines all the servos and usb devices.
 USB Usb;
 XBOXRECV Xbox(&Usb);
+
 Servo motor1;
 Servo motor2;
 
@@ -25,17 +33,20 @@ Servo myservo7;
 Servo myservo8;
 Servo servo; // temp servo controll for switching between servos
 
-bool stabilisationMode = false;
-int movement = 10;
-int timeDelay = 50;
-int pin = 2;
-int temp[10] = {};
+// global variables
+bool withinRange = false; // is the tritrack the correct distance from the stabilisation board
+int movement = 10; // default angle of rotation
+int timeDelay = 50; // default time of rotation per degree
+int pin = 2; // initial servo to control
+int temp[10] = {}; // stores values of servo angles when not being controlled
 
 // Defines FB and LR as float values for sending to the sabertooth.
 float FB;
 float LR;
 
 void setup() {
+  Serial.begin(9600);
+
   motor1.attach(8); // Attaches a pin to a servo.
   motor2.attach(9);
 
@@ -51,7 +62,11 @@ void setup() {
   temp[3] = myservo4.read();
   temp[4] = myservo5.read();
   temp[5] = myservo6.read();
-  Serial.begin(115200);
+  // setup the ultrasonic sensor
+  /* pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+  */
+  // does not work with the ultra sonic sensors
 #if !defined(__MIPSEL__)
   while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
 #endif
@@ -62,22 +77,20 @@ void setup() {
   Serial.print(F("\r\nXbox Wireless Receiver Library Started"));
 }
 
-
-
 void loop() {
+  bool stabilisationMode = false;
+  // Serial.println(F("T--O--P")); // was used in debugging
   Usb.Task();
   if (Xbox.XboxReceiverConnected) {
     for (uint8_t i = 0; i < 4; i++) {
       if (Xbox.Xbox360Connected[i]) {
         if (Xbox.getAnalogHat(LeftHatX, i) > 7500 || Xbox.getAnalogHat(LeftHatX, i) < -7500 || Xbox.getAnalogHat(LeftHatY, i) > 7500 || Xbox.getAnalogHat(LeftHatY, i) < -7500)  {
-
           LR = Xbox.getAnalogHat(LeftHatX, i); // Gets the values from the left analog stick and saves it to a variable.
           FB = Xbox.getAnalogHat(LeftHatY, i);
           LR = LR * -1; // Converts the recieved value from controller to a value between 30 and 150 which is what the servos work off.
           LR = LR / 32768;
           LR = LR * 60;
           LR = LR + 90;
-          motor2.write(LR); // Writes calculated value to servo.
           /*Serial.print(F("LeftHatX: ")); // Used for debugging.
             Serial.print(LR);
             Serial.print("\t");*/
@@ -85,19 +98,19 @@ void loop() {
           FB = FB / 32768;
           FB = FB * 60;
           FB = FB + 90;
-          motor1.write(FB);
           /*Serial.print(F("LeftHatX: "));
             Serial.print(LR);
             Serial.print("\t");*/
+          drive(LR, FB);
         } else {
-          motor1.write(90);
-          motor2.write(90);
+          motor1.write(neutral); // dont turn the motors if nothing from analogue or deadzone
+          motor2.write(neutral);
         }
+
+        //      enter or leave stabilisation mode
         if (Xbox.getButtonClick(BACK, i)) {
-          stabilisationMode = !stabilisationMode;
-          Xbox.setLedOn(LED1, i);
-          if (stabilisationMode == true) {
-            Serial.print(stabilisationMode);
+          stabilisationMode = !stabilisationMode; // enable / disable the stabilisation mode
+          if (stabilisationMode == true)  { // set up servos and distance of tri-track for stabilisation mode
             servoDetach();
             myservo.write(40);
             myservo2.write(90);
@@ -107,9 +120,32 @@ void loop() {
             myservo6.write(90);
             pin = 2;
             servoAttach();
+            Serial.println(withinRange);
+            // code does not want to enter this subroutine, not sure why
+            stabilisationPosition();
+            Serial.println(withinRange);
           }
         }
-        // these two selections allow the servo m
+
+        // if the stabilisation is started and the button is pressed it will toggle the switch
+        // then return to its original stabilisation position
+        if (Xbox.getButtonClick(START, i)) {
+          if (stabilisationMode == true)  {
+            myservo4.write(130); // lifts arm up out of the knob
+            delay(500);
+            myservo6.write(50); // rotates arm to the switch
+            delay(100);
+            myservo4.write(150); // flicks switch
+            delay(100);
+            myservo4.write(130); // raises arm
+            myservo6.write(90);
+            delay(500);
+            myservo4.write(150); // returns arm to original position
+          }
+        }
+/*   
+ *    this code was used to change angle of rotation when using buttons
+        // adjusting by how much the servos will rotate in degrees
         if (Xbox.getButtonClick(UP, i)) {
           Xbox.setLedOn(LED1, i);
           if (movement < 50) {
@@ -119,6 +155,7 @@ void loop() {
             timeDelay = 500 / movement; // takes 500 miliseconds per movment
           }
         }
+        
         if (Xbox.getButtonClick(DOWN, i)) {
           Xbox.setLedOn(LED4, i);
           Serial.print(F("Down: "));
@@ -128,7 +165,8 @@ void loop() {
           }
           Serial.println(movement);
         }
-
+*/
+        // switches to the servo to the left of current servo
         if (Xbox.getButtonClick(L1, i)) {
           servoDetach();
           if (stabilisationMode == true) {
@@ -144,8 +182,8 @@ void loop() {
           servoAttach();
           Serial.print(("L1: "));
           Serial.println(pin);
-        } else if (Xbox.getButtonClick(R1, i)) {
-          Serial.print(stabilisationMode);
+        } else if (Xbox.getButtonClick(R1, i)) {  // switches to the servo to the right of current servo
+          Serial.println(stabilisationMode);
           servoDetach();
           if (stabilisationMode == true) {
             pin = 3;
@@ -161,24 +199,22 @@ void loop() {
           Serial.print(F("R1: "));
           Serial.println(pin);
         }
+        // moves the servos by -5 degrees
+        if (Xbox.getButtonPress(R2, i)) {
+          // moves servos by "movement" degrees over ~500ms
+          servo.write(servo.read() + 1);
+          delay(10);
+        }
+        // moves the servos by +5 degrees
+        if (Xbox.getButtonPress(L2, i)) {
+          servo.write(servo.read() - 1);
+          delay(10);
+        }
 
-        if (Xbox.getButtonClick(A, i)) {
-          for (int i = 0; i < 6; i++) {
-            servo.write(servo.read() - 1);
-            delay(100);
-          }
-          Serial.println(myservo.read());
-        }
-        if (Xbox.getButtonClick(B, i)) {
-          for (int i = 0; i < 6; i++) {
-            servo.write(servo.read() + 1);
-            delay(100);
-          }
-          Serial.write(servo.read());
-        }
+
         if (Xbox.getButtonClick(X, i)) {
           if (stabilisationMode == true) {
-            // anti clockwise
+            // anti clockwise rotation of the knob
             servoDetach();
             myservo2.write(0);
             myservo.write(5);
@@ -190,19 +226,13 @@ void loop() {
             myservo.write(5);
             delay(200);
             servoAttach();
-          } else {
-            for (int i = 0; i < movement; i++) {
-              servo.write(servo.read() - 1);
-              delay(timeDelay);
-            }
-            Serial.println(myservo.read());
           }
-          Serial.write(servo.read());
+          //Serial.write(servo.read());
           //Serial.println(F("X"));
         }
         if (Xbox.getButtonClick(Y, i)) {
           if (stabilisationMode == true) {
-            // clockwise
+            // clockwise rotation of the knob
             servoDetach();
             myservo2.write(90);
             myservo.write(5);
@@ -214,17 +244,12 @@ void loop() {
             myservo.write(5);
             delay(200);
             servoAttach();
-            } else {
-            for (int i = 0; i < movement; i++) {
-              servo.write(servo.read() + 1);
-              delay(timeDelay);
-            }
-
           }
-          Serial.println(myservo.read());
+          //Serial.println(myservo.read());
           //Serial.println(F("Y"));
         }
       }
+
     }
   }
 }
@@ -304,299 +329,36 @@ void servoAttach() {
       break;
   }
 }
-=======
-/*
-  Example sketch for the Xbox Wireless Reciver library - developed by Kristian Lauszus
-  It supports up to four controllers wirelessly
-  For more information see the blog post: http://blog.tkjelectronics.dk/2012/12/xbox-360-receiver-added-to-the-usb-host-library/ or
-  send me an e-mail:  kristianl@tkjelectronics.com
-*/
 
-
-#include <XBOXRECV.h>
-#include <Servo.h>
-
-// Satisfy the IDE, which needs to see the include statment in the ino too.
-#ifdef dobogusinclude
-#include <spi4teensy3.h>
-#endif
-#include <SPI.h>
-
-// Defines all the servos and usb devices.
-USB Usb;
-XBOXRECV Xbox(&Usb);
-Servo motor1;
-Servo motor2;
-
-//Servos
-Servo myservo;
-Servo myservo2;
-Servo myservo3;    // twelve servo objects can be created on most boards
-Servo myservo4;
-Servo myservo5;
-Servo myservo6;
-Servo myservo7;
-Servo myservo8;
-Servo servo; // temp servo controll for switching between servos
-
-int pin = 2;
-int temp[10] = {};
-
-// Defines FB and LR as float values for later use.
-float FB;
-float LR;
-
-void setup() {
-  motor1.attach(8); // Attaches a pin to a servo.
-  motor2.attach(9);
-
-  servo.attach(2);    // attaches the servo owhat voltn pin 9 to the servo object
-  myservo2.attach(3);
-  myservo3.attach(4);
-  myservo4.attach(5);
-  myservo5.attach(6);
-  myservo6.attach(7);
-  temp[0] = servo.read();
-  temp[1] = myservo2.read();
-  temp[2] = myservo3.read();
-  temp[3] = myservo4.read();
-  temp[4] = myservo5.read();
-  temp[5] = myservo6.read();
-  
-  Serial.begin(115200);
-#if !defined(__MIPSEL__)
-  while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
-#endif
-  if (Usb.Init() == -1) {
-    Serial.print(F("\r\nOSC did not start"));
-    while (1); //halt
-  }
-  Serial.print(F("\r\nXbox Wireless Receiver Library Started"));
+void drive(float LeftRight, float FrontBack) {
+  motor2.write(LeftRight); // Writes value to sabtertooth to drive motors
+  motor1.write(FrontBack);
 }
 
 
-
-void loop() {
-  Usb.Task();
-  if (Xbox.XboxReceiverConnected) {
-    for (uint8_t i = 0; i < 4; i++) {
-      if (Xbox.Xbox360Connected[i]) {
-        if (Xbox.getAnalogHat(LeftHatX, i) > 7500 || Xbox.getAnalogHat(LeftHatX, i) < -7500 || Xbox.getAnalogHat(LeftHatY, i) > 7500 || Xbox.getAnalogHat(LeftHatY, i) < -7500)  {
-
-          LR = Xbox.getAnalogHat(LeftHatX, i); // Gets the values from the left analog stick and saves it to a variable.
-          FB = Xbox.getAnalogHat(LeftHatY, i);
-          LR = LR * -1; // Converts the recieved value from controller to a value between 30 and 150 which is what the servos work off.
-          LR = LR / 32768;
-          LR = LR * 60;
-          LR = LR + 90;
-          motor2.write(LR); // Writes calculated value to servo.
-          Serial.print(F("LeftHatX: ")); // Used for debugging.
-          Serial.print(LR);
-          Serial.print("\t");
-          FB = FB * -1; // Same process, but for the forward backward movement.
-          FB = FB / 32768;
-          FB = FB * 60;
-          FB = FB + 90;
-          motor1.write(FB);
-          Serial.print(F("LeftHatX: "));
-          Serial.print(LR);
-          Serial.print("\t");
-        } else {
-          motor1.write(90);
-          motor2.write(90);
-        }
-
-        if (Xbox.getButtonClick(L1, i)) {
-          int temp_ = servo.read();
-          servo.detach();
-          switch (pin) {
-            case 2:
-              myservo.attach(2);
-              myservo.write(temp_);
-              break;
-            case 3:
-              myservo2.attach(3);
-              myservo2.write(temp_);
-              break;
-            case 4:
-              myservo3.attach(4);
-              myservo3.write(temp_);
-              break;
-            case 5:
-              myservo4.attach(5);
-              myservo4.write(temp_);
-              break;
-            case 6:
-              myservo5.attach(6);
-              myservo5.write(temp_);
-              break;
-            case 7:
-              myservo6.attach(7);
-              myservo6.write(temp_);
-              break;
-            default:
-              break;
-          }
-
-          temp[0] = myservo.read();
-          temp[1] = myservo2.read();
-          temp[2] = myservo3.read();
-          temp[3] = myservo4.read();
-          temp[4] = myservo5.read();
-          temp[5] = myservo6.read();
-          if (pin > 2) {
-            pin = pin - 1;
-          }
-          else {
-            pin = 7;
-          }
-
-          Serial.print(("L1: "));
-          Serial.print(pin);
-          switch (pin) {
-            case 2:
-              myservo.detach();
-              servo.attach(pin);
-              servo.write(temp[0]);
-              break;
-            case 3:
-              myservo2.detach();
-              servo.attach(pin);
-              servo.write(temp[1]);
-              break;
-            case 4:
-              myservo3.detach();
-              servo.attach(pin);
-              servo.write(temp[2]);
-              break;
-            case 5:
-              myservo4.detach();
-              servo.attach(pin);
-              servo.write(temp[3]);
-              break;
-            case 6:
-              myservo5.detach();
-              servo.attach(pin);
-              servo.write(temp[4]);
-              break;
-            case 7:
-              myservo6.detach();
-              servo.attach(pin);
-              servo.write(temp[5]);
-              break;
-            default:
-              break;
-          }
-        } else if (Xbox.getButtonClick(R1, i)) {
-          int temp_ = servo.read();
-          servo.detach();
-          switch (pin) {
-            case 2:
-              myservo.attach(2);
-              myservo.write(temp_);
-              break;
-            case 3:
-              myservo2.attach(3);
-              myservo2.write(temp_);
-              break;
-            case 4:
-              myservo3.attach(4);
-              myservo3.write(temp_);
-              break;
-            case 5:
-              myservo4.attach(5);
-              myservo4.write(temp_);
-              break;
-            case 6:
-              myservo5.attach(6);
-              myservo5.write(temp_);
-              break;
-            case 7:
-              myservo6.attach(7);
-              myservo6.write(temp_);
-              break;
-            default:
-              break;
-          }
-
-          temp[0] = myservo.read();
-          temp[1] = myservo2.read();
-          temp[2] = myservo3.read();
-          temp[3] = myservo4.read();
-          temp[4] = myservo5.read();
-          temp[5] = myservo6.read();
-          if (pin < 7) {
-            pin = pin + 1;
-          }
-          else {
-            pin = 2;
-          }
-          Serial.println(F("R1"));
-          switch (pin) {
-            case 2:
-              myservo.detach();
-              servo.attach(pin);
-              servo.write(temp[0]);
-              break;
-            case 3:
-              myservo2.detach();
-              servo.attach(pin);
-              servo.write(temp[1]);
-              break;
-            case 4:
-              myservo3.detach();
-              servo.attach(pin);
-              servo.write(temp[2]);
-              break;
-            case 5:
-              myservo4.detach();
-              servo.attach(pin);
-              servo.write(temp[3]);
-              break;
-            case 6:
-              myservo5.detach();
-              servo.attach(pin);
-              servo.write(temp[4]);
-              break;
-            case 7:
-              myservo6.detach();
-              servo.attach(pin);
-              servo.write(temp[5]);
-              break;
-            default:
-              break;
-          }
-          Serial.println(pin);
-        }
-        if (Xbox.getButtonClick(A, i)) {
-         for (int i = 0; i < 10; i++) {
-          servo.write(servo.read() - 1);
-          delay(50);
-        }
-        }
-        if (Xbox.getButtonClick(B, i))
-        for (int i = 0; i < 10; i++) {
-          servo.write(servo.read() + 1);
-          delay(50);
-        }
-        if (Xbox.getButtonClick(X, i)) {
-          for (int i = 0; i < 6; i++) {
-            servo.write(servo.read() - 1);
-            delay(100);
-          }
-          Serial.println(F("X"));
-        }
-        if (Xbox.getButtonClick(Y, i)) {
-          for (int i = 0; i < 6; i++) {
-            servo.write(servo.read() + 1);
-            delay(100);
-          }
-          Serial.println(F("Y"));
-        }
-      }
-
+void stabilisationPosition() {
+  float distance, duration;
+  while (withinRange == false) {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    return (distance = (duration / 2) / 29.1);
+    if (distance > 12.00) {
+      Serial.println("Too far!");
+      drive(neutral, neutral - FBboard);
+    } else if (distance <= 10.00) {
+      Serial.println("Too close!");
+      drive(neutral, neutral + FBboard);
+    } else {
+      Serial.println(F("Target Reached"));
+      drive(neutral, neutral);
+      withinRange = true;
     }
+    Serial.print(distance);
+    Serial.println(" cm");
+    delay(500);
   }
 }
-
->>>>>>> origin/master

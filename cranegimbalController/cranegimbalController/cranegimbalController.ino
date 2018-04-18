a140b2f16d930973f956e8dcc1c1b8119a37c6da
Harry O'Malley <hazaomalley@gmail.com>
2018-04-18 14:00:18 +0100

    Made projects for different systems

---
+++ cranegimbalController/cranegimbalController/cranegimbalController.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		cranegimbalController.ino
+ Created:	4/18/2018 1:59:58 PM
+ Author:	harryomalley
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
